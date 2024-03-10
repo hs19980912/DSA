b247942bfb14e8feb3309a4802e0223c7e10f631
Himanshu Singh <63532987+hs19980912@users.noreply.github.com>
2024-03-11 01:08:29 +0530

    all combined according to Mind map.
    Refer Whimsical Mind map.
    https://whimsical.com/adobe-BV9AHuB9rwFjxyYKjjD7wZ

---
+++ DP/DP.cpp
@@ -0,0 +1,23 @@
+================================================================
+*/★ GENERAL */ 
+
+1. Longest Arithmetic Progression.
+	Given an array called A[] of sorted integers having no duplicates, find the length of the Longest Arithmetic Progression (LLAP) in it.
+
+	Example 1:
+
+	Input:
+	N = 6
+	set[] = {1, 7, 10, 13, 14, 19}
+	Output: 4
+	Explanation: The longest arithmetic 
+	progression is {1, 7, 13, 19}.
+	Example 2:
+
+	Input:
+	N = 5
+	A[] = {2, 4, 6, 8, 10}
+	Output: 5
+	Explanation: The whole set is in AP.
+
+	
