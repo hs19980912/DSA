0a100d32b616698b303d7cd810825f8742882b87
Himanshu Singh <hs19980912@gmail.com>
2024-09-09 22:21:47 +0530

    Rotate Array

---
+++ Arrays/Rotate Array/Rotate Array.cpp
@@ -0,0 +1,12 @@
+Given an array arr[] of size N, the task is to rotate the array by d position to the left.
+
+Examples: 
+
+Input:  arr[] = {1, 2, 3, 4, 5, 6, 7}, d = 2
+Output: 3, 4, 5, 6, 7, 1, 2
+
+=======================================================================================
+'
+Reverse the first d elements
+Reverse last (N-d) elements
+Reverse the whole array.
