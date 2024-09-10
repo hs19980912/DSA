2ac992e57298709947e1f8b29629a3fe4e700e98
Himanshu Singh <hs19980912@gmail.com>
2024-09-10 23:35:48 +0530

    Alternate positive and negative element

--- Arrays/Rotate Array/Rotate Array.cpp
+++ Arrays/Rotate Array/Rotate Array.cpp
@@ -2,11 +2,11 @@ Given an array arr[] of size N, the task is to rotate the array by d position to
 
 Examples: 
 
-Input:  arr[] = {1, 2, 3, 4, 5, 6, 7}, d = 2
-Output: 3, 4, 5, 6, 7, 1, 2
+Input:     1, 2, 3, 4, 5, 6, 7   d = 2
+Output:    3, 4, 5, 6, 7, 1, 2
 
 =======================================================================================
 '
-Reverse the first d elements
-Reverse last (N-d) elements
-Reverse the whole array.
+Reverse the first d elements   2 1 3 4 5 6 7
+Reverse last (N-d) elements    2 1 7 6 5 4 3
+Reverse the whole array.       3 4 5 6 7 1 2
