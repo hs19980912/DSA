7000fd2531e24628890d6d999836c97700f3e0b4
Himanshu Singh <hs19980912@gmail.com>
2024-09-10 22:20:28 +0530

    Move negative to the left and positives to the right without changing the order

--- Arrays/Negatives to left and positives to the right without changing order/rearrange array without changing order - Sliding Window.cpp
+++ Arrays/Negatives to left and positives to the right without changing order/rearrange array without changing order - Sliding Window.cpp
@@ -25,6 +25,17 @@ In the given array, negative numbers
 are -3, -2 and non-negative numbers are 1, 0.
 ==============================================================================================
 
+Here the window at any given point will only contain positive numbers.
+So now at every iteration we have to add jth index to our window, 2 cases possible:
+    case 1: arr[j] is positive
+            Window is valid and simply move the j++ forward.
+    case 2: arr[j] is negative
+            eg: -1 -4 -3 -6 7 5 3 9 7 5 -2 8 7 5 -3 8 6 4
+                            i            j
+                        Make this to ↓
+                -1 -4 -3 -6 -2 7 5 3 9 7 5 8 7 5 -3 8 6 4  
+                             i           j
+
 class Solution
 {
     public:
