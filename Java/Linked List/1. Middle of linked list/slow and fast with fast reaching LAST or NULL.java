ae874ccd8de7d68e2c65c7894bf0b3b1046921e8
Himanshu Singh <hs19980912@gmail.com>
2024-10-07 22:40:27 +0530

    Middle of the linked List, second middle in case of 2 middles

--- Java/Linked List/1. Middle of linked list/slow and fast with fast reaching LAST or NULL.java
+++ Java/Linked List/1. Middle of linked list/slow and fast with fast reaching LAST or NULL.java
@@ -9,26 +9,23 @@ Input: linked list = 10 -> 20 -> 30 -> 40 -> 50 -> 60
 Output: 40
 Explanation: There are 6 nodes in the linked list, so we have two middle nodes: 30 and 40, but we will return the second middle node which is 40.
 
-[Expected Approach] By Tortoise and Hare Algorithm – O(n) time and O(1) space:
+[Expected Approach] 
+By Tortoise and Hare Algorithm – O(n) time and O(1) space:
 We can use the Hare and Tortoise Algorithm to find the middle of the linked list. Traverse linked list using a slow pointer (slow_ptr) and a fast pointer (fast_ptr ). Move the slow pointer to the next node(one node forward) and the fast pointer to the next of the next node(two nodes forward). When the fast pointer reaches the last node or NULL, then the slow pointer will reach the middle of the linked list.
-
 In case of odd number of nodes in the linked list, slow_ptr will reach the middle node when fast_ptr will reach the last node and in case of even number of nodes in the linked list, slow_ptr will reach the middle node when fast_ptr will become NULL.
 
 ==================================================================
 
-int getMiddle(Node* head) {
-  
-    // Initialize the slow and fast pointer to the head of
-    // the linked list
-    Node* slow_ptr = head;
-    Node* fast_ptr = head;
-
-    while (fast_ptr != NULL && fast_ptr->next != NULL) {
-        fast_ptr = fast_ptr->next->next;
-        slow_ptr = slow_ptr->next;
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
     }
-
-    // Return the slow pointer which is currenlty pointing
-    // to the middle node of the linked list
-    return slow_ptr->data;
 }
