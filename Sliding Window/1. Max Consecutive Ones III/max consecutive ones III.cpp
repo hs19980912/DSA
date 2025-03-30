a4e7fa70fbea1fc7589f221b03e3cf1a1b9a2560
Himanshu Singh <63532987+hs19980912@users.noreply.github.com>
2025-03-31 01:49:05 +0530

    revert lol!

--- Sliding Window/1. Max Consecutive Ones III/max consecutive ones III.cpp
+++ Sliding Window/1. Max Consecutive Ones III/max consecutive ones III.cpp
@@ -1,30 +1,6 @@
 1004. Max Consecutive Ones III
 
 Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
-
-=============================================================================
-
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> m; 
-        int i = 0;
-        int j = 0;
-        int ans = 0;
-        int n = s.size();
-
-        while(j<n){
-            m[s[j]]+=1;
-            while(m[s[j]]>1 && i<=j){
-                m[s[i]]-=1;
-                i+=1;
-            }
-
-            ans = max(j-i+1, ans);
-            j+=1;
-        }
-
-        return ans;
-    }
-
 =============================================================================
 
     int longestOnes(vector<int>& nums, int k) {
