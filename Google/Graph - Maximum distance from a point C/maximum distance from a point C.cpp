dd7bbee842c1d9bc7cf6e41e1895c89e5f19a76c
Himanshu Singh <63532987+hs19980912@users.noreply.github.com>
2025-04-23 11:45:13 +0530

    Graph traversa from point A to Bl by keep a maximum distance from a point C

---
+++ Google/Graph - Maximum distance from a point C/maximum distance from a point C.cpp
@@ -0,0 +1,7 @@
+
+There is a N×N grid with some water cells that can't be crossed, and movement is restricted to the four directions, up, down, left and right.
+A mouse is trying to get from its starting position S to a piece of cheese at T, but there’s a cat at position C. The goal is to find a path from S to T where the shortest distance to the cat C along the path is as large as possible. Use Manhattan distance to measure how close the mouse gets to the cat.
+
+
+Link: https://leetcode.com/discuss/post/6679126/google-onsite-interview-by-6npqo57mii-i6te/
+
